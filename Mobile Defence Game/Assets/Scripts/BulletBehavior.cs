7dcbfa67a639244c50ea69cacac6fd36cb871dd7
damonkim80 <42937396+damonkim80@users.noreply.github.com>
2019-12-09 16:33:55 +0900

    bulletstat 총알 클래스 생성 및 변경

--- Mobile Defence Game/Assets/Scripts/BulletBehavior.cs
+++ Mobile Defence Game/Assets/Scripts/BulletBehavior.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour {
 
-    public float speed = 10.0f;
+    public BulletStat bulletStat { get; set; }
+
+    public BulletBehavior()
+    {
+        bulletStat = new BulletStat(0, 0);
+    }
+
     public GameObject character;
 
-    private int damage;
-
-    public void setDamage(int input)
-    {
-        damage = input;
-    }
 
 	void Start () {
         Destroy(gameObject, 3.0f);
 	}
 	
 	void Update () {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        transform.Translate(Vector2.right * bulletStat.speed * Time.deltaTime);
 	}
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -27,7 +27,7 @@ public class BulletBehavior : MonoBehaviour {
         if(other.gameObject.tag == "Monster")
         {
             Destroy(gameObject);
-            other.GetComponent<MonsterStat>().attacked(damage);
+            other.GetComponent<MonsterStat>().attacked(bulletStat.damage);
         }   
     }
 }
